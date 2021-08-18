2c8ebb0209222847910f511bfec415ec5a48aa87
ArleyTheNerd <87275311+ArleyTheNerd@users.noreply.github.com>
2021-08-18 17:12:02 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,7 +3,28 @@ import os
 from keep_alive import keep_alive
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='>')
+class CustomHelpCommand(commands.HelpCommand):
+
+  def __init__(self):
+    super().__init__()
+ 
+async def send_bot_help(self, mapping):
+  for cog in mapping:
+    await self.get_destination().send(f'(cog.qualified_name): {[command.name for command in mapping[cog]]}')
+
+async def send_cog_help(self, cog):
+  await self.get_Destination().send(f'(cog.qualified_name): {[command.name for command in cog.get_commands()]}')
+
+async def send_group_help(self, group):
+  await self.get_Destination().send(f'{group.name}: {[command.name for index, command in enumerate(group.commands)]}')
+
+
+async def send_command_help(self, command):
+   await self.get_destination().send(command.name)  
+
+   
+client = commands.Bot(command_prefix = '>', help_command=commands.MinimalHelpCommand())
+
 
 @client.event
 async def on_ready():
